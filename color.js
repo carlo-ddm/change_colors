51cd9a545aa9c0ead1727ee71521c79cc88ae43b
carlo-ddm <carloduminuco90@gmail.com>
2022-09-09 20:10:34 +0200

    agguyngo audio a funzione di cambio colore (bottone)

--- color.js
+++ color.js
@@ -1,9 +1,22 @@
-const chenge_color_btn = document.getElementById('change_color')
-chenge_color_btn.addEventListener('click',chenge_color)
+const chenge_color_btn = document.getElementById('change_color');
+chenge_color_btn.addEventListener('click',chenge_color);
+var audio = new Audio('mixkit-water-sci-fi-bleep-902.wav')
 
 function chenge_color() {
+  audio.play();
   const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  console.log(randomColor);
 
-  let color = document.body.style.backgroundColor = "#" + randomColor
+  let color = document.body.style.backgroundColor = "#" + randomColor;
   color.innerHTML = "#" + randomColor;
+  console.log(color);
 }
+
+// const d = new Date;
+// let minutes = d.getMinutes()
+// function auto_change(m) {
+//   m++
+//   if (m++){
+//     chenge_color()
+//   }
+// }
